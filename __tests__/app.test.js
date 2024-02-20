30b3a6e698ce2be685988693126b2944cba39551
iainrm290 <murdochiain17@gmail.com>
2024-02-20 13:38:29 +0000

    completed GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,47 +8,92 @@ const endpointsFile = require('../endpoints.json')
 beforeEach(() => seed(data))
 afterAll(() => db.end())
 
-describe ('app', () => {
+describe('app', () => {
     describe('GET /api/topics', () => {
         test('200: responds with status 200 for successful request', () => {
             return request(app)
-            .get('/api/topics')
-            .expect(200)
+                .get('/api/topics')
+                .expect(200)
         })
         test('responds with an array of objects each containing a slug and description property', () => {
             return request(app)
-            .get('/api/topics')
-            .then((response) => {
-                const topics = response.body.topics
-                expect(topics).toHaveLength(3)
-                topics.forEach((topic) => {
-                    expect(topic).toHaveProperty('slug', expect.any(String))
-                    expect(topic).toHaveProperty('description', expect.any(String))
+                .get('/api/topics')
+                .then((response) => {
+                    const topics = response.body.topics
+                    expect(topics).toHaveLength(3)
+                    topics.forEach((topic) => {
+                        expect(topic).toHaveProperty('slug', expect.any(String))
+                        expect(topic).toHaveProperty('description', expect.any(String))
+                    })
                 })
-            })
         })
     })
     describe('GET /api', () => {
         test('responds with an object containing all available endpoints', () => {
             return request(app)
-            .get('/api')
-            .expect(200)
-            .then((response) => {
-                const endPoints = response.body.endpoints
-                expect(endPoints).toEqual(endpointsFile)
-            })
+                .get('/api')
+                .expect(200)
+                .then((response) => {
+                    const endPoints = response.body.endpoints
+                    expect(endPoints).toEqual(endpointsFile)
+                })
         })
         test('each endpoint contains a description, queries, and exampleResponse property', () => {
             return request(app)
-            .get('/api')
-            .expect(200)
+                .get('/api')
+                .expect(200)
+                .then((response) => {
+                    const endPoints = response.body.endpoints
+                    for (const value in endPoints) {
+                        expect(endPoints[value]).toHaveProperty('description', expect.any(String))
+                        expect(endPoints[value]).toHaveProperty('queries', expect.any(Array))
+                        expect(endPoints[value]).toHaveProperty('exampleResponse', expect.any(Object))
+                    }
+                })
+        })
+    })
+    describe('GET /api/articles/:article_id', () => {
+        test('200: responds with status 200 for successful request', () => {
+            return request(app)
+                .get('/api/articles/1')
+                .expect(200)
+        })
+        test('responds with the article object which matches the requested article id', () => {
+            return request(app)
+                .get('/api/articles/1')
+                .expect(200)
+                .then((response) => {
+                    const article = response.body.article
+                    const articleOne = {
+                        article_id: 1,
+                        title: "Living in the shadow of a great man",
+                        topic: "mitch",
+                        author: "butter_bridge",
+                        body: "I find this existence challenging",
+                        created_at: '2020-07-09T20:11:00.000Z',
+                        votes: 100,
+                        article_img_url:
+                            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+                    }
+                    expect(article).toEqual(articleOne)
+                })
+        })
+        test('400: responds with a 400 error for an invalid request', () => {
+            return request(app)
+            .get('/api/articles/music')
+            .expect(400)
             .then((response) => {
-                const endPoints = response.body.endpoints
-                for (const value in endPoints) {
-                    expect(endPoints[value]).toHaveProperty('description', expect.any(String))
-                    expect(endPoints[value]).toHaveProperty('queries', expect.any(Array))
-                    expect(endPoints[value]).toHaveProperty('exampleResponse', expect.any(Object))
-                }
+                const err = response.body
+                expect(err.msg).toBe('bad request')
+            })
+        })
+        test('404: responds with a 404 for a valid id which does not exist', () => {
+            return request(app)
+            .get('/api/articles/1000')
+            .expect(404)
+            .then((response) => {
+                const errMsg = response.body.msg
+                expect(errMsg).toBe('no article with that id')
             })
         })
     })
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getAllTopics, getAllEndpoints } = require('./controllers/app.controller')
+const { getAllTopics, getAllEndpoints, getArticleById } = require('./controllers/app.controller')
 const app = express()
 
 app.use(express.json())
@@ -8,14 +8,23 @@ app.get('/api/topics', getAllTopics)
 
 app.get('/api', getAllEndpoints)
 
+app.get('/api/articles/:article_id', getArticleById)
+
 app.use((err, request, response, next) => {
     if (err.status && err.msg) {
         response.status(err.status).send({msg: err.msg})
+    } else {
+        next(err)
     }
 })
 
 app.use((err, request, response, next) => {
-    response.status(500).send({ msg: 'Internal server error' })
+    if (err.code === '22P02') {
+        response.status(400).send({msg: 'bad request'})
+    } else {
+        response.status(500).send({ msg: 'Internal server error' })
+    }
+    
 })
 
 module.exports = app
